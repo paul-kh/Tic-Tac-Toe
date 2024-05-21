82f206aa57e00bc6b56206e128005e3494fa9f32
Paul Chheang <phalcheat@gmail.com>
2024-05-20 23:56:20 -0400

    Changed player symbol

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <div id="game-container">
         <ol id="players">
           <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
         GAME BOARD
       </div>
